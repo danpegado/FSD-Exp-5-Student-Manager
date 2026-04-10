2af7cf2ed17a4d097fcbb3d79f1b7c88226e5eba
danpegado <danpegado@users.noreply.github.com>
2026-04-10 08:49:43 +0530

    Prepared Neural Student Manager for Vercel deployment

--- api/server.js
+++ api/server.js
@@ -159,6 +159,4 @@ app.delete("/deleteUser/:id", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-	console.log(`🚀 Server running on port ${PORT}`);
-});
+module.exports = app;
